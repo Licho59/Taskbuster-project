d926839e37029cd1f50f78e010d47ecd970aea4f
Licho59 <lestlalka@gmail.com>
2018-04-12 13:35:52 +0200

    Improvements made like coverage tests or favicon icon but the last one does not work on Safari.

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
-from .views import home
+from .views import home, home_files
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home, name='home')
+    path('home/', home, name='home'),
+    re_path(r'^(?P<filename>(robots.txt)|(humans.txt))$', home_files, name="home-files"),
 ]
